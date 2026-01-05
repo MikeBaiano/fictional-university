368074a12bf7e2dd54b194a0e46562949f7cc887
MikeBaiano <mike.queen1@gmail.com>
2026-01-05 14:34:15 -0300

    Programs Begin

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_post_types() {
+    // Event Post Type
     register_post_type( 'event', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
@@ -14,6 +15,23 @@ function university_post_types() {
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event'
+        ),
+    ) );
+    
+    // Program Post Type
+    register_post_type( 'program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'programs'),
+        'menu_icon' => 'dashicons-awards',
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
         ),
     ) );
 }
