9e233cce834494645cbaa275e0baf1e6ebca2530
MikeBaiano <mike.queen1@gmail.com>
2026-01-05 19:32:34 -0300

    Update single-program.php

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -23,7 +23,6 @@ while ( have_posts() ) {
       <div class="generic-content">
         <p><?php the_content(); ?></p>
       </div>
-
       <?php 
             $today = date('Ymd');
             $homepageEvents = new WP_Query( array(
@@ -42,26 +41,32 @@ while ( have_posts() ) {
                 array(
                   'key' => 'related_programs',
                   'compare' => 'LIKE',
-                  'value' => get_the_ID()
+                  'value' => '"' . get_the_ID() . '"'
                 )
               )
             ) );
-            while ( $homepageEvents->have_posts() ) {
-              $homepageEvents->the_post(); ?>
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                  <span class="event-summary__month"><?php 
-                  $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('M');
-                   ?></span>
-                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php echo wp_trim_words(get_the_excerpt(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+
+            if ($homepageEvents->have_posts()) {
+              echo '<hr class="section-break">';
+              echo '<h2 class="headline headline--medium" style="margin-bottom: 45px;">Upcoming ' . get_the_title() . ' Events</h2>';
+
+              while ( $homepageEvents->have_posts() ) {
+                $homepageEvents->the_post(); ?>
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                    ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                  </div>
                 </div>
-              </div>
-          <?php
+            <?php
+              }
             } wp_reset_postdata();
           ?>
       </div>
