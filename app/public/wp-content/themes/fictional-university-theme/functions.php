4dd7652717ecef5a0913f4857c500e9bb14ab138
MikeBaiano <mike.queen1@gmail.com>
2026-01-06 13:59:56 -0300

    Fazendo PageBanner

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,32 @@
 <?php
 
+function pageBanner($args = array()) {
+  if (!isset($args['title'])) {
+    $args['title'] = get_the_title();
+  }
+  if (!isset($args['subtitle'])) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+  if (!isset($args['image'])) {
+    if (get_field('page_banner_background_image')) {
+      $args['image'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['image'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+    ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['image']; ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle']; ?></p>
+        </div>
+      </div>
+    </div>
+    <?php
+}
+
 function university_files() {
     wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/build/index.js' ), array('jquery'), '1.0', true );
     wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
@@ -15,6 +42,10 @@ function university_features() {
     register_nav_menu('footerLocationOne', 'Footer Location One' );
     register_nav_menu('footerLocationTwo', 'Footer Location Two' );
     add_theme_support( 'title-tag' );
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action( 'after_setup_theme', 'university_features' );
--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -1,4 +1,5 @@
-<?php get_header(); ?>
+<?php 
+get_header(); ?>
 
 <?php
 
@@ -24,6 +25,39 @@ while ( have_posts() ) {
         <p><?php the_content(); ?></p>
       </div>
       <?php 
+
+            $relatedProfessors = new WP_Query( array(
+              'posts_per_page' => -1,
+              'post_type' => 'professor',
+              'orderby' => 'title',
+              'order' => 'ASC',
+              'meta_query' => array(
+                array(
+                  'key' => 'related_programs',
+                  'compare' => 'LIKE',
+                  'value' => '"' . get_the_ID() . '"'
+                )
+              )
+            ) );
+
+            if ($relatedProfessors->have_posts()) {
+              echo '<hr class="section-break">';
+              echo '<h2 class="headline headline--medium" style="margin-bottom: 45px;">' . get_the_title() . ' Professors</h2>';
+              echo '<ul class="professor-cards">'; 
+              while ( $relatedProfessors->have_posts() ) {
+                $relatedProfessors->the_post(); ?>
+                <li class="professor-card__list-item">
+                  <a class="professor-card" href="<?php the_permalink(); ?>">
+                    <img class="professor-card__image" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'professorLandscape'); ?>" alt="">
+                    <span class="professor-card__name"><?php the_title(); ?></span>
+                  </a>
+                </li>
+
+            <?php
+              }
+            echo '</ul>';
+            } wp_reset_postdata();            
+
             $today = date('Ymd');
             $homepageEvents = new WP_Query( array(
               'posts_per_page' => 2,
