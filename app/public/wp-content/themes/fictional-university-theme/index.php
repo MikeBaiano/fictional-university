4a4bfadae6cd3b1bcfc97b6939b32a5b8ef60b97
MikeBaiano <mike.queen1@gmail.com>
2026-01-07 11:35:46 -0300

    duplicate code
    Getting rid of

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,16 +1,11 @@
 <?php 
 
-get_header(); ?>
+get_header(); 
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our Blog</h1>
-        <div class="page-banner__intro">
-          <p>Keep up with the latest news and updates from our university community.</p>
-        </div>
-      </div>
-    </div>
+pageBanner(array(
+  'title' => 'Welcome to our Blog',
+  'subtitle' => 'Keep up with the latest news and updates from our university community.',
+)); ?>
     
     <div class="container container--narrow page-section">
       <?php
--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -4,16 +4,9 @@ get_header(); ?>
 <?php
 
 while ( have_posts() ) {
-    the_post(); ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Dont forget to replace me later.</p>
-        </div>
-      </div>
-    </div>
+    the_post(); 
+    pageBanner();
+    ?>
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
@@ -86,19 +79,7 @@ while ( have_posts() ) {
 
               while ( $homepageEvents->have_posts() ) {
                 $homepageEvents->the_post(); ?>
-                <div class="event-summary">
-                  <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"><?php 
-                    $eventDate = new DateTime(get_field('event_date'));
-                    echo $eventDate->format('M');
-                    ?></span>
-                    <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                  </a>
-                  <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_excerpt(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                  </div>
-                </div>
+                <?php get_template_part('template-parts/event'); ?>
             <?php
               }
             } wp_reset_postdata();
