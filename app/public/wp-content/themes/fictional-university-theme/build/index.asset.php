5e94e3b701d87a9eefd9e4e906efade176680f46
MikeBaiano <mike.queen1@gmail.com>
2026-01-15 10:55:11 -0300

    Advanced Search
    Search all kind of posts and cathegorized response

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '2093c53145e734ed0903');
+<?php return array('dependencies' => array('jquery'), 'version' => 'a25a800446d1e1867c75');
---
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -0,0 +1,69 @@
+<?php
+
+add_action('rest_api_init','universityRegisterSearch');
+
+function universityRegisterSearch() {
+    register_rest_route('university/v1', 'search', array(
+        'methods' => 'GET',
+        'callback' => 'universitySearchResults'
+    ));
+}
+
+function universitySearchResults($data) {
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'event', 'campus'),
+        's' => sanitize_text_field($data['term']),
+        'posts_per_page' => -1
+    ));
+
+    $results = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array()
+    );
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+        if (get_post_type() == 'post' || get_post_type() == 'page') {
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'postType' => get_post_type(),
+                'authorName' => get_the_author()
+            ));
+        }
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(get_the_ID(), 'professorLandscape'),
+            ));
+        }
+        if (get_post_type() == 'program') {
+            array_push($results['programs'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+            ));
+        }
+        if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'date' => $eventDate,
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'excerpt' => wp_trim_words(get_the_excerpt(), 10),
+            ));
+        }
+        if (get_post_type() == 'campus') {
+            array_push($results['campuses'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+            ));
+        }
+    }
+
+    return $results;
+}
